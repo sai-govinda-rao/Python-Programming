a0f28e95401eead753a1c4bb0db7a838b60d1578
Kornu Sai Govinda Rao <kornusaigovinda@gmail.com>
2024-07-23 19:11:25 +0530

    Data Types in Python

---
+++ Data_type.py
@@ -0,0 +1,20 @@
+# in Python programming language we don't need to mention the data type
+# Python will itself to indicate the type of the data
+a = 10
+b = 10.0
+c = "Hello"
+d = True
+e = 10+30j
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+"""
+<class 'int'>
+<class 'float'>
+<class 'str'>
+<class 'bool'>
+<class 'complex'>
+"""
