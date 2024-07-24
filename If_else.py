7d884cdaa00970aca8371f354ac85f860f0c3f4c
Kornu Sai Govinda Rao <kornusaigovinda@gmail.com>
2024-07-24 15:23:51 +0530

    Add files via upload

---
+++ If_else.py
@@ -0,0 +1,35 @@
+# DecisionMaking
+# indentation is
+"""
+If Conditions Is True Then Execute block of code
+if condition:   If condition satisfies then execute block of code
+    ......
+    ......
+    ......
+    ......
+"""
+#Ex:
+a = 10
+b = 20
+if a < b:
+    print(f"{a} is less then {b}")
+"""
+else if alternative of if
+If Condition is not satisfies then execute else block
+if condition:
+    .......
+    .......  #if condition True
+    .......
+else:
+    .......
+    .......  # if condition False 
+    .......
+"""
+#Ex:
+
+age = int(input("Enter your Age:"))
+if age >= 18:
+    print("you are eligible to vote")
+else:
+    print("you are not eligible to vote")
+
